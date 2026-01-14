123f540a340220c83660d565d261544671f892d1
Riniinfanceya <156685015+Riniinfanceya@users.noreply.github.com>
2026-01-14 08:32:30 +0530

    Create home_screen.dart

---
+++ lib/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard")),
+      body: const Center(
+        child: Text(
+          "Welcome to Flutter Firebase App",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
